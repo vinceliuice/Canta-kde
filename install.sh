14c6ade3cce6d7aefee9a49374c35ed753ccdcb9
vinceliuice <vinceliuice@hotmail.com>
2019-06-26 17:08:29 +0800

    Add sddm theme

--- install.sh
+++ install.sh
@@ -15,14 +15,14 @@ install() {
   local name=${1}
   local color=${2}
 
-  [[ ${color} == '-dark' ]] && local ELSE_DARK=${color}
-  [[ ${color} == '-light' ]] && local ELSE_LIGHT=${color}
+  [[ ${color} == '-dark' ]] && local ELSE_COLOR='dark'
+  [[ ${color} == '-light' ]] && local ELSE_COLOR='light'
 
-  local AURORAE_THEME=${AURPRAE_DIR}/${name}-light
+  local AURORAE_THEME=${AURPRAE_DIR}/${name}${color}
   local PLASMA_THEME=${PLASMA_DIR}/${name}${color}
   local LOOKFEEL_THEME=${LOOKFEEL_DIR}/${name}${color}
-  local SCHEMES_THEME=${SCHEMES_DIR}/${name}${color}.colors
-  local KVANTUM_THEME=${KVANTUM_DIR}/${name}-light
+  local SCHEMES_THEME=${SCHEMES_DIR}/${name}${ELSE_COLOR}.colors
+  local KVANTUM_THEME=${KVANTUM_DIR}/${name}${color}
 
   [[ -d ${AURORAE_THEME} ]] && rm -rf ${AURORAE_THEME}
   [[ -d ${PLASMA_THEME} ]] && rm -rf ${PLASMA_THEME}
@@ -30,11 +30,11 @@ install() {
   [[ -d ${SCHEMES_THEME} ]] && rm -rf ${SCHEMES_THEME}
   [[ -d ${KVANTUM_THEME} ]] && rm -rf ${KVANTUM_THEME}
 
-  cp -ur ${SRC_DIR}/aurorae/themes/${name}-light                                     ${AURORAE_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/${name}${color}.colors                             ${SCHEMES_DIR}
-  cp -ur ${SRC_DIR}/Kvantum/${name}-light                                            ${KVANTUM_DIR}
+  cp -ur ${SRC_DIR}/aurorae/${name}${color}                                          ${AURORAE_DIR}
+  cp -ur ${SRC_DIR}/color-schemes/${name}${ELSE_COLOR}.colors                        ${SCHEMES_DIR}
+  cp -ur ${SRC_DIR}/Kvantum/${name}${color}                                          ${KVANTUM_DIR}
   cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${color}                              ${PLASMA_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/${name}${color}.colors                             ${PLASMA_THEME}/colors
+  cp -ur ${SRC_DIR}/color-schemes/${name}${ELSE_COLOR}.colors                        ${PLASMA_THEME}/colors
   cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}      ${LOOKFEEL_DIR}
 }
 
